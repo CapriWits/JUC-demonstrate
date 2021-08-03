0124b9380a2b048c26ad1221af52e85f457f1351
hypocrite30 <1027093394@qq.com>
2021-08-04 00:07:14 +0800

    feat: update src

---
+++ src/JUC-Demonstrate/Java8/src/main/java/com/hypocrite30/_8/TestAqs.java
@@ -0,0 +1,107 @@
+package com.hypocrite30._8;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+import static com.hypocrite30._2.util.Sleeper.sleep;
+
+/**
+ * @Description: 用 AQS 自定义锁，不可重入
+ * @Author: Hypocrite30
+ * @Date: 2021/8/3 11:06
+ */
+@Slf4j(topic = "c.TestAqs")
+public class TestAqs {
+    public static void main(String[] args) {
+        MyLock lock = new MyLock();
+        new Thread(() -> {
+            lock.lock();
+            try {
+                log.debug("locking...");
+                sleep(1);
+            } finally {
+                log.debug("unlocking...");
+                lock.unlock();
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                log.debug("locking...");
+            } finally {
+                log.debug("unlocking...");
+                lock.unlock();
+            }
+        }, "t2").start();
+    }
+}
+
+// 自定义锁（不可重入锁）
+class MyLock implements Lock {
+
+    // 独占锁  同步器类
+    class MySync extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                // 加上了锁，并设置 owner 为当前线程
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            setExclusiveOwnerThread(null);
+            setState(0); // state 是 volatile，放在后面，可以让前面的操作对其他线程可见
+            return true;
+        }
+
+        @Override // 是否持有独占锁
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        public Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private MySync sync = new MySync();
+
+    @Override // 加锁（不成功会进入等待队列）
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override // 加锁，可打断
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override // 尝试加锁（一次）
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override // 尝试加锁，带超时
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override // 解锁
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override // 创建条件变量
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
